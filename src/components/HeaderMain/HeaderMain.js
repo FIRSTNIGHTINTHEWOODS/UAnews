d46c68a0845264d5b50b04847af075500e132dcc
Serhii Lukynov <serhii.lukynov.payever@gmail.com>
2019-09-23 12:47:12 +0300

    axios, initial

---
+++ src/components/HeaderMain/HeaderMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeaderMain.scss';
+
+function HeaderMain() {
+    return (
+        <div className="header-main">
+            <div className="logo">
+                L
+            </div>
+        </div>
+    );
+}
+export default HeaderMain;
